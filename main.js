a54a5387eec457bc1ca8b6e3b7294abf01dd0989
sanix-darker <s4nixd@gmail.com>
2020-01-24 02:58:49 +0100

    mdr, it's working !

---
+++ main.js
@@ -0,0 +1,21 @@
+///////////------------------------------------------------
+/////////// BerryCast -------------------------------------
+///////////------------------------------------------------
+
+const { app, BrowserWindow, ipcMain } = require('electron')
+const path = require('path')
+
+// declare this as a variable globally so we can
+// reference it and so it will not be garbage collected
+let mainWindow
+
+// wait for the main process to be ready
+app.on('ready', () => {
+  // path to our html
+  const htmlPath = path.join('src', 'index.html')
+
+  // create a browser window
+  mainWindow = new BrowserWindow()
+
+  mainWindow.loadFile(htmlPath)
+})
---
+++ src/renderer.js
@@ -0,0 +1,6 @@
+const MY_ANDROID_IP_ADDRESS = "http://192.168.43.1:8080"
+
+// A simple redirection to the Cast on my mobile
+setTimeout(function(){
+  location.href = MY_ANDROID_IP_ADDRESS
+}, 10000);
